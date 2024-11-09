8d323aa94e7cb1a22cece3fa127605c0413d2dc7
cleveng <cleveng@gmail.com>
2024-11-09 18:33:25 +0800

    fix: --wip--

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -3,9 +3,13 @@ import { View, Text } from '@tarojs/components'
 import { ref } from 'vue'
 
 const msg = ref('Hello world')
+const onClick = () => {
+  console.log('[Navbar]: on click title')
+}
 </script>
 <template>
   <View>
+    <nut-navbar title="登录" safe-area-inset-top left-show @click-back="onClick"> </nut-navbar>
     <Text>{{ msg }}</Text>
   </View>
 </template>
