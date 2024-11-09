74081ca8049d4f2b41d628dcdaa1f043a6f94aec
cleveng <cleveng@gmail.com>
2024-11-09 18:03:47 +0800

    perf: add login page

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   animation: true,
   entryPagePath: 'pages/blank/index',
-  pages: ['pages/index/index', 'pages/blank/index', 'pages/home/index'],
+  pages: ['pages/index/index', 'pages/blank/index', 'pages/home/index', 'pages/login/index'],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
---
+++ src/pages/login/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { View, Text } from '@tarojs/components'
+import { ref } from 'vue'
+
+const msg = ref('Hello world')
+</script>
+<template>
+  <View>
+    <Text>{{ msg }}</Text>
+  </View>
+</template>
